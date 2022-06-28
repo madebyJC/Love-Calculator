c5b8b0e3a88cc77cef2126378d4135367c5e2e32
Jc Paredes <108071426+yourJC@users.noreply.github.com>
2022-06-28 22:35:46 +0800

    removed comments

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-# 🚨 Don't change the code below 👇
 print("Welcome to the Love Calculator!")
 name1 = input("What is your name? \n")
 name2 = input("What is their name? \n")
-# 🚨 Don't change the code above 👆
-
-#Write your code below this line 👇
 
 combined_names = name1 + name2
 lower_names = combined_names.lower()
